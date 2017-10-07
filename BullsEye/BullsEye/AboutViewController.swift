62a0dedfb5d1bad1fe4edb655e80b7db2a13e9be
nat <n.lunina@live.ru>
2017-10-07 04:34:57 +0400

    About screen

---
+++ BullsEye/BullsEye/AboutViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Nat on 07.10.17.
+//  Copyright © 2017 Nat. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
